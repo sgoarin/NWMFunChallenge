841782067da377cf14ccf7bd55bdd8201fed9f52
sgoarinab <stephane.goarin@abdata.com>
2018-05-13 17:54:01 -0500

    spinner

--- NWMFunChallenge/src/app/app.module.ts
+++ NWMFunChallenge/src/app/app.module.ts
@@ -1,42 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { CommonModule } from '@angular/common';  
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
 import { GetThoseBeastsComponent } from './get-those-beasts/get-those-beasts.component';
 import { GetThoseBeastsService } from './get-those-beasts.service';
 import { FormsModule } from '@angular/forms';
-
+import { Ng4LoadingSpinnerModule } from 'ng4-loading-spinner';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
-
-
-import { Interceptor } from './classes/interceptor';
 import { ConfigurationService } from './configuration.service';
-import { FilterPipe } from './classes/FilterPipe';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     MainComponent,
-    GetThoseBeastsComponent,
-    FilterPipe
+    GetThoseBeastsComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     FormsModule,
-    CommonModule
+    CommonModule,
+    Ng4LoadingSpinnerModule.forRoot() 
   ],
   providers: [GetThoseBeastsService    //ConfigurationService,
-    //{
-    //  provide: HTTP_INTERCEPTORS,
-    //  useClass: Interceptor,
-    //  multi: true
-    //},
-
-
   ],
   bootstrap: [AppComponent]
 })
--- NWMFunChallenge/src/app/get-those-beasts/get-those-beasts.component.ts
+++ NWMFunChallenge/src/app/get-those-beasts/get-those-beasts.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
 import { GetThoseBeastsService } from '../get-those-beasts.service';
 import { BeastApi } from '../classes/BeastApi';
+import { Ng4LoadingSpinnerService } from 'ng4-loading-spinner';
 
 @Component({
   selector: 'app-get-those-beasts',
@@ -10,46 +10,41 @@ import { BeastApi } from '../classes/BeastApi';
 })
 export class GetThoseBeastsComponent implements OnInit {
 
-  constructor(private getThoseBeastsService: GetThoseBeastsService) { }
+  constructor(private getThoseBeastsService: GetThoseBeastsService, private spinnerService: Ng4LoadingSpinnerService) { }
 
   public categories: string[] = [];
   public displayedRecords: string[] = [];
   public searchResults: string[] = [];
   searchActive: boolean = false;
-  
   backEnabled: boolean = false;
   nextEnabled: boolean = true;
-
   startIndex: number = 0;
   endIndex: number = 10;
   numberOfrecords: number = 10;
 
+  sortOrder: number = 1;
+
   ngOnInit() {
-    this.getData();
+    this.spinnerService.show();
+    this.getThoseBeastsService.getTheBeasts().subscribe
+      (
+      res => {
+        console.log(res);
+        this.categories = res;
+        if (this.categories.length < this.numberOfrecords) {
+          this.endIndex = this.categories.length;
+        }
+        this.displayedRecords = this.categories.slice(this.startIndex, this.endIndex);
+      },
+
+      error => {
+        console.log("error =" + error);
+      });
+    this.spinnerService.hide();
   }
 
-  getData() {
-      
-      this.getThoseBeastsService.getTheBeasts().subscribe
-        (
-        res => {
-          this.categories = res;
-          if (this.categories.length < this.numberOfrecords) {
-            this.endIndex = this.categories.length;
-          }
-          this.displayedRecords = this.categories.slice(this.startIndex, this.endIndex);
-
-          //this.spinnerService.hide();
-        },
-
-        error => {
-          console.log("error =" + error);
-          //this.spinnerService.hide();
-        });
-   }
-
-  next() {
-
+   next() {
+    this.spinnerService.show();
     if (!this.searchActive) {
       this.searchResults = this.categories;
     }
@@ -66,15 +61,17 @@ export class GetThoseBeastsComponent implements OnInit {
     }
 
     this.backEnabled = true;
-    this.displayedRecords = this.searchResults.slice(this.startIndex, this.endIndex);
+    this.displayData();
 
     if (this.searchResults.length <= this.endIndex)
     { this.nextEnabled = false; }
     else
     { this.nextEnabled = true; }
+    this.spinnerService.hide();
   }
 
   back() {
+    this.spinnerService.show();
     this.startIndex = this.startIndex - this.numberOfrecords;
     this.endIndex = this.endIndex - this.numberOfrecords;
 
@@ -84,49 +81,72 @@ export class GetThoseBeastsComponent implements OnInit {
     if (this.searchResults.length <= this.endIndex) { this.nextEnabled = false; }
     else { this.nextEnabled = true; }
 
-    this.displayedRecords = this.searchResults.slice(this.startIndex, this.endIndex);
+    this.displayData();
+    this.spinnerService.hide();
   }
 
   search(searchText: string) {
-    this.getThoseBeastsService.getTheBeasts().subscribe
-      (
-      res => {
-        this.categories = res;
-        if (this.categories) {
-          if (!searchText) {
-            this.searchResults = this.categories;
 
-            this.searchActive = false;
-            this.startIndex = 0;
-            this.endIndex = 10;
-            this.displayedRecords = this.searchResults.slice(this.startIndex, this.endIndex);
-           
-          }
-          else {
-            searchText = searchText.toLowerCase();
-            this.searchActive = true;
-            this.searchResults = this.categories.filter(cat => {
-              return cat.toLowerCase().startsWith(searchText)
-            });
-            this.displayedRecords = this.searchResults.slice(0, 10);
-          }
+    this.startIndex = 0;
+    this.endIndex = 10;
 
-          this.backEnabled = false;
-          if (this.searchResults.length <= this.endIndex)
-          { this.nextEnabled = false; }
-            else { this.nextEnabled = true; }
-        }
-         
-      },
-
-      error => {
-        console.log("error =" + error);
-        //this.spinnerService.hide();
-      });
-
+    if (this.categories) {
+      if (!searchText) {
+        this.searchResults = this.categories;
+        this.searchActive = false;
+      }
+      else {
+        searchText = searchText.toLowerCase();
+        this.searchActive = true;
+        this.searchResults = this.categories.filter(cat => {
+          return cat.toLowerCase().startsWith(searchText)
+        });
+      }
+      this.displayData();
+      
+      this.backEnabled = false;
+      if (this.searchResults.length <= this.endIndex)
+        { this.nextEnabled = false; }
+          else { this.nextEnabled = true; }
+    }
+    this.spinnerService.hide();
   }
 
-  
+  sort() {
+    this.spinnerService.show();
+
+
+    this.sortOrder = this.sortOrder * -1;
+
+    if (!this.searchActive) {
+      this.searchResults = this.categories;
+    }
+
+    if (this.sortOrder > 0) {
+      this.searchResults.sort();
+    }
+    else {
+      this.searchResults.reverse();
+    }
+    this.startIndex = 0;
+    this.endIndex = 10;
+    
+    this.displayData();
+    
+    this.spinnerService.hide();
+  }
+
+  reset(searchValue: string) {
+    if (!searchValue) {
+      this.search(null);
+    }
+  }
+
+  displayData() {
+    this.displayedRecords = this.searchResults.slice(this.startIndex, this.endIndex);
+  }
+
+
   }
 
 
